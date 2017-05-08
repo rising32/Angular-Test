3a74d1f2f3c994bb609d2ac76e411b367364c119
jace <jace.dev.1986@yandex.com>
2017-05-09 04:12:18 +0800

    add order, description, sub menu

--- public/js/website.js
+++ public/js/website.js
@@ -11,15 +11,15 @@ angular.module('website', ['ngRoute', 'ui.bootstrap']).
     .controller('HomeCtrl', function ($scope, $http, $q, $location) {
         $scope.news = [
             {
-                title: 'News Item1', img: 'http://lorempixel.com/400/200/'
+                title: 'News Item1', img: 'http://lorempixel.com/400/200/', date: 1469959781804, description: 'Description 1 Note: ng-click needs to call a function, or execute some code. You could write a function literal to set the property, but this is mixing controller logic into the view (think about what would happen if the sort needed to change later, you would need to update that logic in the view, which is bad).I added a sort function to handle this, that just takes the property. You can set this up however you need to'
             }, {
-                title: 'News Item2', img: 'http://lorempixel.com/400/200/food'
+                title: 'News Item2', img: 'http://lorempixel.com/400/200/food', date: 1468029434776, description: 'Description 2 Note: ng-click needs to call a function, or execute some code. You could write a function literal to set the property, but this is mixing controller logic into the view (think about what would happen if the sort needed to change later, you would need to update that logic in the view, which is bad).I added a sort function to handle this, that'
             }, {
-                title: 'News Item3', img: 'http://lorempixel.com/400/200/sports'
+                title: 'News Item3', img: 'http://lorempixel.com/400/200/sports', date: 1469192218634, description: 'Description 3 I added a sort function to handle this, that just takes the property. You can set this up however you need to'
             }, {
-                title: 'News Item4', img: 'http://lorempixel.com/400/200/people'
+                title: 'News Item4', img: 'http://lorempixel.com/400/200/people', date: 1469157574527, description: 'Description 4'
             }];
-        $scope.currentNews = $scope.news[0].img;
+
         $scope.myInterval = 3000;
         $scope.slides = [
             {
@@ -36,7 +36,21 @@ angular.module('website', ['ngRoute', 'ui.bootstrap']).
             }
         ]; 
 
-        $scope.changeCurrentView = function(img) {
-            $scope.currentNews = img;
+        $scope.sortFactor = 'title';
+        $scope.currentNews = 0;
+        $scope.visibleDescription = true;
+
+        $scope.changeCurrentView = function(index) {
+            if(index == $scope.currentNews) {
+                $scope.visibleDescription = !$scope.visibleDescription;
+            } else {
+                $scope.visibleDescription = true;
+            }
+            $scope.currentNews = index;
+        }
+
+        $scope.showDate = function(d) {
+            var date = new Date(d);
+            return date.getFullYear() + ' / ' + (date.getMonth() + 1) + ' / ' + (date.getDay() + 1);
         }
     })
